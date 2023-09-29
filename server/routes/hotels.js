d83ec56408da0eed3b5ee92c2dc6ae9537091556
diyanshu <diyanshu0360@gmail.com>
2023-09-29 18:01:32 +0530

    Done Final

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -79,18 +79,18 @@ router.get("/countByCity", async (req, res) => {
 // TYPE COUNT
 router.get("/countByType", async (req, res) => {
   try {
-    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
-    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
-    const resortCount = await Hotel.countDocuments({ type: "resort" });
-    const villaCount = await Hotel.countDocuments({ type: "villa" });
-    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+    const hotelCount = await Hotel.countDocuments({ type: "Hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "Apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "Resort" });
+    const villaCount = await Hotel.countDocuments({ type: "Villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "Cabin" });
     
     res.status(200).json([
-      { type: "hotel", count: hotelCount },
-      { type: "apartments", count: apartmentCount },
-      { type: "resorts", count: resortCount },
-      { type: "villas", count: villaCount },
-      { type: "cabins", count: cabinCount },
+      { type: "Hotel", count: hotelCount },
+      { type: "Apartments", count: apartmentCount },
+      { type: "Resort", count: resortCount },
+      { type: "Villa", count: villaCount },
+      { type: "Cabin", count: cabinCount },
     ]);
   } catch (error) {
     res.status(500).json(error);
